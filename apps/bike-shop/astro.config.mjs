a644e419e718c412c335c022cd1326de9946ae83
Kevin I Rivera <kevin.ivan05@gmail.com>
2023-02-16 14:24:11 -0600

    add sitemap

--- apps/bike-shop/astro.config.mjs
+++ apps/bike-shop/astro.config.mjs
@@ -1,10 +1,12 @@
 import { defineConfig } from 'astro/config';
 import vercel from '@astrojs/vercel/serverless';
 import lit from '@astrojs/lit';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
     output: 'server',
-    integrations: [lit()],
+    site: 'https://enduro4150.com',
+    integrations: [lit(), sitemap()],
     adapter: vercel()
 });
