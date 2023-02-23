d0e5222a7bbb8eaec1597ce4ee1d42ec60af55c3
Kevin I Rivera <kevin.ivan05@gmail.com>
2023-02-23 13:16:03 -0600

    add trailing slash per documentation

--- apps/bike-shop/astro.config.mjs
+++ apps/bike-shop/astro.config.mjs
@@ -6,10 +6,10 @@ import sitemap from '@astrojs/sitemap';
 // https://astro.build/config
 export default defineConfig({
     output: 'server',
-    site: 'https://www.enduro4150.com',
+    site: 'https://www.enduro4150.com/',
     integrations: [lit(), sitemap({
         customPages: [
-            'https://www.enduro4150.com', 
+            'https://www.enduro4150.com/', 
             'https://www.enduro4150.com/registro',
             'https://www.enduro4150.com/eventos',
             'https://www.enduro4150.com/nosotros',
