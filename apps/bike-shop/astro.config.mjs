6451d4202b91470a2efce48f0a16afa016b8d1be
Kevin I Rivera <kevin.ivan05@gmail.com>
2023-02-23 18:11:08 -0600

    replace url with new domain

--- apps/bike-shop/astro.config.mjs
+++ apps/bike-shop/astro.config.mjs
@@ -6,13 +6,13 @@ import sitemap from '@astrojs/sitemap';
 // https://astro.build/config
 export default defineConfig({
     output: 'server',
-    site: 'https://www.enduro4150.com/',
+    site: 'https://www.serialmtbtexcoco.com/',
     integrations: [lit(), sitemap({
         customPages: [
-            'https://www.enduro4150.com/', 
-            'https://www.enduro4150.com/registro',
-            'https://www.enduro4150.com/eventos',
-            'https://www.enduro4150.com/nosotros',
+            'https://www.serialmtbtexcoco.com/', 
+            'https://www.serialmtbtexcoco.com/registro',
+            'https://www.serialmtbtexcoco.com/eventos',
+            'https://www.serialmtbtexcoco.com/nosotros',
         ]
     })],
     adapter: vercel()
