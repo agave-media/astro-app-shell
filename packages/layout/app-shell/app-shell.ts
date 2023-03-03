f61ec72cd1ea5f2b88135ffb62fa6e4a071aa430
Kevin I Rivera <kevin.ivan05@gmail.com>
2023-03-02 22:50:03 -0600

    revert header button to less prominent kind

--- packages/layout/app-shell/app-shell.ts
+++ packages/layout/app-shell/app-shell.ts
@@ -6,7 +6,7 @@ import "../app-drawer/app-drawer";
 import "../app-header/app-header";
 import "../app-bottom-navigation/app-bottom-navigation";
 import "@material/web/fab/fab";
-import "@material/web/button/filled-link-button";
+import "@material/web/button/outlined-link-button";
 import "@material/web/icon/icon";
 import "@material/web/iconbutton/standard-icon-button";
 import "@material/web/iconbutton/standard-icon-button-toggle";
@@ -121,9 +121,9 @@ export class AppShell extends LitElement {
                     </div>
                 </md-standard-icon-button-toggle>
                 
-                <md-filled-link-button href="/consulta" slot="trailing" label="Revisar boleto">
+                <md-outlined-link-button class="secondary" href="/consulta" slot="trailing" label="Revisar boleto">
                     <md-icon slot="icon">bolt</md-icon>
-                </md-filled-link-button>
+                </md-outlined-link-button>
                 <!-- ${this.loggedIn ? html`
                     <md-outlined-button @click=${this.signOut} slot="trailing" label="MiSerial">
                         <md-icon slot="icon">bolt</md-icon>
